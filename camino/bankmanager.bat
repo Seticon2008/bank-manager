f9d5e23add11a086d17d6212a1339eb6fb8981ad
Seticon2008 <danielbehrens02@gmail.com>
2025-10-02 21:16:02 -0500

    Release 1.1.0

--- camino/bankmanager.bat
+++ camino/bankmanager.bat
@@ -1,3 +1,4 @@
 @echo off
-javac C:\programs\camino\Camino.java
-java -cp C:\programs\camino Camino
+cd C:\programs\camino
+javac C:\programs\camino\BankOpInterface.java
+java -cp C:\programs\camino BankOpInterface
---
+++ camino/bankviewer.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\programs\camino
+javac C:\programs\camino\BankInterface.java
+java -cp C:\programs\camino BankInterface
